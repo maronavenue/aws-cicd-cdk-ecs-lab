743584286c0c2520b2c8add6e2d9c14d8691507b
EC2 Default User <ec2-user@ip-172-31-14-162.ap-southeast-1.compute.internal>
2020-11-11 08:46:41 +0000

    add build pipeline

--- lib/myapp-stack.ts
+++ lib/myapp-stack.ts
@@ -1,9 +1,109 @@
 import * as cdk from '@aws-cdk/core';
+import * as codecommit from '@aws-cdk/aws-codecommit';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecsPatterns from '@aws-cdk/aws-ecs-patterns';
+import * as ecr from '@aws-cdk/aws-ecr';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipelineActions from '@aws-cdk/aws-codepipeline-actions';
 
 export class MyappStack extends cdk.Stack {
+  private codeRepository: codecommit.Repository;
+  private cluster: ecs.Cluster;
+  private service: ecs.BaseService;
+  private imageRepository: ecr.Repository;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.createCodeCommitRepository();
+    this.createECSApplication();
+    this.createECRRepository();
+    this.createPipeline();
+  }
+  
+  createCodeCommitRepository() {
+    this.codeRepository = new codecommit.Repository(this, 'CodeRepository', {
+      repositoryName: 'MyRepository'
+    });
+  }
+  
+  createECSApplication() {
+    this.cluster = new ecs.Cluster(this, 'Cluster');
+    const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
+      cluster: this.cluster,
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset('.'),
+        containerName: 'web',
+      },
+    });
+    
+    this.service = fargateService.service;
+  }
+  
+  createECRRepository() {
+    this.imageRepository = new ecr.Repository(this, 'ImageRepository');
+    new cdk.CfnOutput(this, 'ImageRepositoryURI', { value: this.imageRepository.repositoryUri });
+  }
+  
+  createPipeline() {
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      stages: this.createPipelineStages(),
+    });
+  }
+  
+  createPipelineStages() {
+    const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+    return [
+      this.createStageSource(sourceOutput),
+      this.createStageBuild(sourceOutput, buildOutput),
+    ];
+  }
+  
+  createStageSource(output: codepipeline.Artifact): codepipeline.StageOptions {    
+    const action = new codepipelineActions.CodeCommitSourceAction({
+      actionName: 'CodeCommit',
+      repository: this.codeRepository,
+      output: output,
+    });
+    return {
+      stageName: 'Source',
+      actions: [
+        action    
+      ]
+    };
+  }
+  
+  createStageBuild(input: codepipeline.Artifact, output: codepipeline.Artifact): codepipeline.StageOptions {
+    const project = new codebuild.PipelineProject(this, 'ImageBuildProject', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
+        privileged: true,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec.yml'),
+      environmentVariables: {
+        REPOSITORY_URI: {value: this.imageRepository.repositoryUri},
+        CONTAINER_NAME: {value: "web"},
+      }
+    });
+
+    this.imageRepository.grantPullPush(project.grantPrincipal);
+    
+    const action = new codepipelineActions.CodeBuildAction({
+      actionName: 'ImageBuildAction',
+      input: input,
+      outputs: [output],
+      project: project,
+    });
+    
+    return {
+      stageName: 'Build',
+      actions: [
+        action
+      ]
+    };
   }
 }
